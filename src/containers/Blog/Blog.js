abd2e7e6e6a7c1e06bd5de0220594faaaa6e35f0
Emmanuel <emmychris477@gmail.com>
2022-08-11 10:25:43 +0100

    transformed derived data to taste

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -13,8 +13,13 @@ class Blog extends Component {
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return { ...post, author: "Emmy" };
+      });
+
       this.setState({
-        posts: response.data,
+        posts: updatedPosts,
       });
       console.log(response);
     });
@@ -22,7 +27,7 @@ class Blog extends Component {
 
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return <Post key={post.id} title={post.title} author={post.author} />;
     });
 
     return (
